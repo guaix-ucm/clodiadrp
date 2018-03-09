c4a6387398cc5857974b658f5cf510bc0426173d
Sergio Pascual <sergiopr@fis.ucm.es>
2018-03-09 12:23:22 +0100

    Add loader and drp.yaml

---
+++ clodiadrp/loader.py
@@ -0,0 +1,7 @@
+
+
+import numina.core
+
+def drp_load():
+    """Entry point to load CLODIA DRP."""
+    return numina.core.drp_load('clodiadrp', 'drp.yaml')
---
+++ setup.py
@@ -0,0 +1,41 @@
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='clodiadrp',
+    version='1.0.dev0',
+    author='Sergio Pascual',
+    author_email='sergiopr@fis.ucm.es',
+    url='https://github.com/guaix-ucm/numina',
+    license='MIT',
+    description='CLODIA, example Data Reduction Pipeline for Numina',
+    packages=find_packages(),
+    package_data={
+        'clodiadrp': [
+            'drp.yaml',
+
+        ],
+    },
+    install_requires=[
+        'numina >= 0.15',
+    ],
+    entry_points={
+        'numina.pipeline.1': [
+            'CLODIA = clodiadrp.loader:load_drp',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        'Development Status :: 3 - Alpha',
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        ],
+    long_description=open('README.md').read()
+)
